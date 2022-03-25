d2bb19dcac98daa179a6bfb1ffdaff2d763d9c90
Shockch4rge <favouriteteo@gmail.com>
2022-03-25 14:45:29 +0800

    Moved option-relevant tests to MessageCommandOption

--- src/__tests__/MessageCommandBuilder.spec.ts
+++ src/__tests__/MessageCommandBuilder.spec.ts
@@ -1,16 +1,9 @@
-import {
-    Collection, GuildMember, GuildMemberRoleManager, Message, Permissions, Role
-} from "discord.js";
+import { Collection, Message, Permissions, Role } from "discord.js";
 
-import { roleMention } from "@discordjs/builders";
-
-import { MessageCommandBuilder, MessageCommandOption, MessageCommandOptionType } from "../";
-import { MessageCommandStringOption } from "../MessageCommandOption";
-import { StringParser } from "../StringParser";
+import { MessageCommandBuilder, MessageCommandOptionType } from "../";
 
 
 const PREFIX = ">>";
-const parser = new StringParser();
 
 describe("MessageCommandBuilder constructing and testing", () => {
 	it("throw errors for setting missing/incorrect properties", () => {
@@ -54,124 +47,33 @@ describe("MessageCommandBuilder constructing and testing", () => {
 		builder.addBooleanOption(option =>
 			option.setName("boolean").setDescription("boolean option description")
 		);
-		builder.addMentionableOption(option =>
+		builder.addMemberOption(option =>
 			option.setName("mentionable").setDescription("mentionable option description")
 		);
 
-		const mockMessage = ">>test-name string 12 true <@!2323829327>";
-		const optionTypeValues = mockMessage
-			.trim()
-			.split(" ")
-			.slice(1)
-			.map(option => parser.parse(option));
+		const mockMessage = {
+			content: '>>test-name "string" 12 true <@!2323829327>',
+			member: {
+				roles: {
+					cache: new Collection<string, Role>(),
+				},
+				permissions: new Permissions(),
+			},
+		} as Message;
+
+		const { options } = builder.validate(mockMessage);
 
 		expect(builder.options.length).toBe(4);
 		expect(builder.options[0].type).toBe(MessageCommandOptionType.STRING);
 		expect(builder.options[1].type).toBe(MessageCommandOptionType.NUMBER);
 		expect(builder.options[2].type).toBe(MessageCommandOptionType.BOOLEAN);
-		expect(builder.options[3].type).toBe(MessageCommandOptionType.MENTIONABLE);
+		expect(builder.options[3].type).toBe(MessageCommandOptionType.MEMBER);
 
-		expect(optionTypeValues[0]).toBe("string");
-		expect(optionTypeValues[1]).toBe(12);
-		expect(optionTypeValues[2]).toBe(true);
-		expect(optionTypeValues[3]).toBe("<@!2323829327>");
-	});
-
-	it("test single string option with regex", () => {
-		const builder = new MessageCommandBuilder();
-		builder.setName("test-name");
-		builder.setDescription("test description");
-		builder.addStringOption(option =>
-			option.setName("test-option-name").setDescription("test option description")
-		);
-
-		const mockMessage = ">>test-name option1";
-		const regex = builder.toRegex(PREFIX);
-
-		expect(builder.name).toBe("test-name");
-		expect(builder.description).toBe("test description");
-		expect(builder.options.length).toBe(1);
-		expect(builder.options[0].name).toBe("test-option-name");
-		expect(builder.options[0].description).toBe("test option description");
-		expect(builder.options[0].type).toBe(MessageCommandOptionType.STRING);
-		expect(regex).toEqual(/^>>(test-name)\s(\w+)$/gm);
-		expect(regex.test(mockMessage)).toBe(true);
-	});
-
-	it("test multiple aliases and a single string option with regex", () => {
-		const builder = new MessageCommandBuilder();
-		builder.setName("test-name");
-		builder.setDescription("test description");
-		builder.setAliases(["t", "TEST"]);
-		builder.addStringOption(option =>
-			option.setName("test-option-name").setDescription("test option description")
-		);
-
-		const mockMessages = [">>test-name option1", ">>TEST option1", ">>t option1"];
-		const expectedRegex = /^>>(test-name|t|TEST)\s(\w+)$/gm;
-		const regex = builder.toRegex(PREFIX);
-
-		expect(builder.name).toBe("test-name");
-		expect(builder.description).toBe("test description");
-		expect(builder.aliases.length).toBe(2);
-		expect(builder.aliases[0]).toBe("t");
-		expect(builder.aliases[1]).toBe("TEST");
-		expect(builder.options.length).toBe(1);
-		expect(builder.options[0].name).toBe("test-option-name");
-		expect(builder.options[0].description).toBe("test option description");
-		expect(builder.options[0].type).toBe(MessageCommandOptionType.STRING);
-		expect(builder.toRegex(PREFIX)).toEqual(expectedRegex);
-
-		mockMessages.forEach(msg => expect(!!msg.match(regex)).toBe(true));
-	});
-
-	it("test all option types with regex", () => {
-		const builder = new MessageCommandBuilder();
-		builder.setName("test-name");
-		builder.setDescription("test description");
-		builder.setAliases(["t", "TEST"]);
-		builder.addStringOption(option =>
-			option.setName("test-option-name").setDescription("test option description")
-		);
-		builder.addNumberOption(option =>
-			option.setName("test-number-option-name").setDescription("test number option description")
-		);
-		builder.addBooleanOption(option =>
-			option.setName("test-boolean-option-name").setDescription("test boolean option description")
-		);
-		builder.addMentionableOption(option =>
-			option
-				.setName("test-mentionable-option-name")
-				.setDescription("test mentionable option description")
-		);
-		builder.addChannelOption(option =>
-			option.setName("test-channel-option-name").setDescription("test channel option description")
-		);
-
-		const expectedRegex = /^>>(test-name|t|TEST)\s(\w+)\s(\d+)\s(true)\s<@!?(\d+)>\s<#(\d+)>$/gm;
-
-		expect(builder.name).toBe("test-name");
-		expect(builder.description).toBe("test description");
-		expect(builder.aliases.length).toBe(2);
-		expect(builder.aliases[0]).toBe("t");
-		expect(builder.aliases[1]).toBe("TEST");
-		expect(builder.options.length).toBe(5);
-		expect(builder.options[0].name).toBe("test-option-name");
-		expect(builder.options[0].description).toBe("test option description");
-		expect(builder.options[0].type).toBe(MessageCommandOptionType.STRING);
-		expect(builder.options[1].name).toBe("test-number-option-name");
-		expect(builder.options[1].description).toBe("test number option description");
-		expect(builder.options[1].type).toBe(MessageCommandOptionType.NUMBER);
-		expect(builder.options[2].name).toBe("test-boolean-option-name");
-		expect(builder.options[2].description).toBe("test boolean option description");
-		expect(builder.options[2].type).toBe(MessageCommandOptionType.BOOLEAN);
-		expect(builder.options[3].name).toBe("test-mentionable-option-name");
-		expect(builder.options[3].description).toBe("test mentionable option description");
-		expect(builder.options[3].type).toBe(MessageCommandOptionType.MENTIONABLE);
-		expect(builder.options[4].name).toBe("test-channel-option-name");
-		expect(builder.options[4].description).toBe("test channel option description");
-		expect(builder.options[4].type).toBe(MessageCommandOptionType.CHANNEL);
-		expect(builder.toRegex(PREFIX)).toEqual(expectedRegex);
+		expect(options.length).toBe(4);
+		expect(options[0]).toBe("string");
+		expect(options[1]).toBe(12);
+		expect(options[2]).toBe(true);
+		expect(options[3]).toBe("2323829327");
 	});
 
 	it("expect 0 errors from validate() method", () => {
@@ -181,14 +83,28 @@ describe("MessageCommandBuilder constructing and testing", () => {
 		builder.setPermissions(["ADD_REACTIONS"]);
 		builder.setRoles(["1234567890"]);
 		builder.addStringOption(option =>
-			option.setName("test-option-name").setDescription("test option description")
+			option.setName("test-string-option-name").setDescription("test string option description")
+		);
+		builder.addNumberOption(option =>
+			option.setName("test-number-option-name").setDescription("test number option description")
+		);
+		builder.addBooleanOption(option =>
+			option.setName("test-boolean-option-name").setDescription("test boolean option description")
+		);
+		builder.addMemberOption(option =>
+			option.setName("test-member-option-name").setDescription("test member option description")
+		);
+		builder.addChannelOption(option =>
+			option.setName("test-channel-option-name").setDescription("test channel option description")
 		);
 
 		const message = {
-			content: ">>test this",
+			content: '>>test "this" 12 true <@!1234567890> <#1234567890>',
 			guild: {
 				roles: {
-					cache: new Collection<string, Role>().set("not-supposed-to-have", null).set("supposed-to-have", null),
+					cache: new Collection<string, Role>()
+						.set("not-supposed-to-have", null)
+						.set("supposed-to-have", null),
 				},
 			},
 			member: {
@@ -199,8 +115,17 @@ describe("MessageCommandBuilder constructing and testing", () => {
 			},
 		} as Message;
 
-		expect(builder.validate(message, PREFIX)).toHaveLength(0);
+		const { errors, options } = builder.validate(message);
+
+		expect(errors.permission).toHaveLength(0);
+		expect(errors.role).toHaveLength(0);
+		expect(errors.option).toHaveLength(0);
+
+		expect(options.length).toBe(5);
+		expect(options[0]).toBe("this");
+		expect(options[1]).toBe(12);
+		expect(options[2]).toBe(true);
+		expect(options[3]).toBe("1234567890");
+		expect(options[4]).toBe("1234567890");
 	});
 });
-
-
--- src/__tests__/MessageCommandOption.spec.ts
+++ src/__tests__/MessageCommandOption.spec.ts
@@ -1,6 +1,7 @@
-import { MessageCommandBuilder, MessageCommandOptionType, MessageCommandStringOption } from '../';
+import { MessageCommandBuilder, MessageCommandOptionType, MessageCommandStringOption } from "../";
 
-const PREFIX = ">>"
+
+const PREFIX = ">>";
 
 describe("MessageCommandOption constructing and testing", () => {
 	it("throw errors for setting missing/incorrect properties", () => {
@@ -28,6 +29,103 @@ describe("MessageCommandOption constructing and testing", () => {
 		).toThrow("You must provide a name and value for every option choice.");
 	});
 
+	it("test single string option with regex", () => {
+		const builder = new MessageCommandBuilder();
+		builder.setName("test-name");
+		builder.setDescription("test description");
+		builder.addStringOption(option =>
+			option.setName("test-option-name").setDescription("test option description")
+		);
+
+		const mockMessage = '>>test-name "option1"';
+		const regex = builder.toRegex(PREFIX);
+
+		expect(builder.name).toBe("test-name");
+		expect(builder.description).toBe("test description");
+		expect(builder.options.length).toBe(1);
+		expect(builder.options[0].name).toBe("test-option-name");
+		expect(builder.options[0].description).toBe("test option description");
+		expect(builder.options[0].type).toBe(MessageCommandOptionType.STRING);
+		expect(regex).toEqual(/^>>(test-name)\s+"(.+)"$/gm);
+		expect(regex.test(mockMessage)).toBe(true);
+	});
+
+	it("test multiple aliases and a single string option with regex", () => {
+		const builder = new MessageCommandBuilder();
+		builder.setName("test-name");
+		builder.setDescription("test description");
+		builder.setAliases(["t", "TEST"]);
+		builder.addStringOption(option =>
+			option.setName("test-option-name").setDescription("test option description")
+		);
+
+		const mockMessages = ['>>test-name "option1"', '>>TEST "option1"', '>>t "option1"'];
+		const expectedRegex = /^>>(test-name|t|TEST)\s+"(.+)"$/gm;
+		const regex = builder.toRegex(PREFIX);
+
+		expect(builder.name).toBe("test-name");
+		expect(builder.description).toBe("test description");
+		expect(builder.aliases.length).toBe(2);
+		expect(builder.aliases[0]).toBe("t");
+		expect(builder.aliases[1]).toBe("TEST");
+		expect(builder.options.length).toBe(1);
+		expect(builder.options[0].name).toBe("test-option-name");
+		expect(builder.options[0].description).toBe("test option description");
+		expect(builder.options[0].type).toBe(MessageCommandOptionType.STRING);
+		expect(builder.toRegex(PREFIX)).toEqual(expectedRegex);
+
+		mockMessages.forEach(msg => expect(!!msg.match(regex)).toBe(true));
+	});
+
+	it("test all option types with regex", () => {
+		const builder = new MessageCommandBuilder();
+		builder.setName("test-name");
+		builder.setDescription("test description");
+		builder.setAliases(["t", "TEST"]);
+		builder.addStringOption(option =>
+			option.setName("test-option-name").setDescription("test option description")
+		);
+		builder.addNumberOption(option =>
+			option.setName("test-number-option-name").setDescription("test number option description")
+		);
+		builder.addBooleanOption(option =>
+			option.setName("test-boolean-option-name").setDescription("test boolean option description")
+		);
+		builder.addMemberOption(option =>
+			option
+				.setName("test-mentionable-option-name")
+				.setDescription("test mentionable option description")
+		);
+		builder.addChannelOption(option =>
+			option.setName("test-channel-option-name").setDescription("test channel option description")
+		);
+
+		const expectedRegex = /^>>(test-name|t|TEST)\s+"(.+)"\s+(\d+)\s+(true)\s+<@!?(\d+)>\s+<#(\d+)>$/gm;
+
+		expect(builder.name).toBe("test-name");
+		expect(builder.description).toBe("test description");
+		expect(builder.aliases.length).toBe(2);
+		expect(builder.aliases[0]).toBe("t");
+		expect(builder.aliases[1]).toBe("TEST");
+		expect(builder.options.length).toBe(5);
+		expect(builder.options[0].name).toBe("test-option-name");
+		expect(builder.options[0].description).toBe("test option description");
+		expect(builder.options[0].type).toBe(MessageCommandOptionType.STRING);
+		expect(builder.options[1].name).toBe("test-number-option-name");
+		expect(builder.options[1].description).toBe("test number option description");
+		expect(builder.options[1].type).toBe(MessageCommandOptionType.NUMBER);
+		expect(builder.options[2].name).toBe("test-boolean-option-name");
+		expect(builder.options[2].description).toBe("test boolean option description");
+		expect(builder.options[2].type).toBe(MessageCommandOptionType.BOOLEAN);
+		expect(builder.options[3].name).toBe("test-mentionable-option-name");
+		expect(builder.options[3].description).toBe("test mentionable option description");
+		expect(builder.options[3].type).toBe(MessageCommandOptionType.MEMBER);
+		expect(builder.options[4].name).toBe("test-channel-option-name");
+		expect(builder.options[4].description).toBe("test channel option description");
+		expect(builder.options[4].type).toBe(MessageCommandOptionType.CHANNEL);
+		expect(builder.toRegex(PREFIX)).toEqual(expectedRegex);
+	});
+
 	it("specify multiple string choices and test with regex", () => {
 		const builder = new MessageCommandBuilder();
 		builder.setName("test-name");
@@ -44,7 +142,7 @@ describe("MessageCommandOption constructing and testing", () => {
 				])
 		);
 
-		const expectedRegex = /^>>(test-name)\s(choice-1|choice-2|choice-3)$/gm;
+		const expectedRegex = /^>>(test-name)\s+"(choice-1|choice-2|choice-3)"$/gm;
 
 		expect(builder.name).toBe("test-name");
 		expect(builder.description).toBe("test description");
