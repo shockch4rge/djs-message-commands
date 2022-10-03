ec5cf3592d3493456c90628609a72d2f0ea6b81f
Favian <favouriteteo@gmail.com>
2022-10-04 00:39:45 +0800

    Allow null prefixes and changed parameters to object

--- src/util/parseMessage.ts
+++ src/util/parseMessage.ts
@@ -5,13 +5,14 @@
  * @param greedy Whether or not to match against one or more whitespaces between arguments in the message command
  * @returns An object containing the command name, raw string arguments, and whether or not the message command was prefixed correctly
  */
-export function parseMessage(content: string, prefix: string, greedy: boolean = true) {
+export function parseMessage(options: { content: string, prefix: string | null, greedySpaces?: boolean }) {
+    const { content, prefix, greedySpaces: greedy = true } = options;
     const greedySpace = greedy ? /\s+/ : /\s/;
     const rawArgs = content.trim().split(greedySpace);
 
     return {
         rawArgs,
-        commandName: rawArgs[0].slice(prefix.length),
-        isPrefixed: rawArgs[0].slice(0, prefix.length) === prefix,
+        commandName: prefix ? rawArgs[0].slice(prefix.length) : "",
+        isPrefixed: rawArgs[0].slice(0, prefix?.length) === prefix,
     }
 }
