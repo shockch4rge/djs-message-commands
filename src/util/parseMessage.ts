432f742aa9e1d81f5a04d13ca7d450910408c54c
Favian <favouriteteo@gmail.com>
2022-10-04 02:05:00 +0800

    Fixed JSDoc

--- src/util/parseMessage.ts
+++ src/util/parseMessage.ts
@@ -1,10 +1,9 @@
 /**
- * A utility function that returns helpful properties from a message command.
+ * A utility function that returns helpful properties from a raw message.
  * @param content The content of the message
- * @param prefix The prefix to match against
- * @param greedy Whether or not to match against one or more whitespaces between arguments in the message command
- * @returns An object containing the command name, raw string arguments, and whether or not the message command was prefixed correctly
- */
+ * @param prefix The prefix to match against. If omitted, `isPrefixed` will be `false`.
+ * @param greedy Whether or not to match against one or more whitespaces between arguments in the message command. Defaults to `true`
+*/
 export function parseMessage(options: { content: string, prefix: string | null, greedySpaces?: boolean }) {
     const { content, prefix, greedySpaces: greedy = true } = options;
     const greedySpace = greedy ? /\s+/ : /\s/;
